02478c455dbad545fdc2d351a20e86005b2363e1
Erick Murillo Molina <erickmurillo22@gmail.com>
2020-09-07 15:21:37 -0600

    mapa index y liderazgos

--- web/views.py
+++ web/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import *
+import datetime
 
 # Create your views here.
 def index(request,template='index.html'):
 	banner = Banner.objects.order_by('-id')[:3]
 	actualidad = Actualidad.objects.order_by('-id')[:6]
+	hoy = datetime.date.today()
+	eventos = Evento.objects.filter(inicio__gte = hoy).order_by('inicio')[:1]
+	liderazgos = Liderazgo.objects.order_by('-id')[:4]
 	return render(request, template, locals())
