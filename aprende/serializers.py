fe6bb1db6d913d8a329d2b6c6cd5a988540c2527
Erick Murillo <erickmurillo22@gmail.com>
2021-02-09 15:56:17 -0600

    --

--- aprende/serializers.py
+++ aprende/serializers.py
@@ -1,7 +1,7 @@
 from .models import *
 from rest_framework import serializers
 
-SEARCH_PATTERN = 'href=\\"/media/ckeditor/'
+SEARCH_PATTERN = 'href=\\"/media/uploads/'
 SITE_DOMAIN = "http://ampb.caps-nicaragua.org"
 REPLACE_WITH = 'href=\\"%s/media/uploads/' % SITE_DOMAIN
 
