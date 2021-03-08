305aa1f7e02b4081d64e279197547ffc8475da21
Erick Murillo <erickmurillo22@gmail.com>
2021-03-08 15:54:45 -0600

    detalle nucleo

--- monitoreo/urls.py
+++ monitoreo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('nucleos/', nucleos, name='nucleos'),
+    path('nucleos/<slug>/', detalle_nucleo, name='detalle-nucleo'),
+    path('participantes/', participantes, name='participantes'),
     path('formadores/', formadores, name='formadores'),
 ]
--- monitoreo/views.py
+++ monitoreo/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from .models import *
+from web.models import *
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
 # Create your views here.
+def nucleos(request,template='nuestro-trabajo/nucleos.html'):
+    escuelas = Escuela.objects.all()
+    #graf jovenes
+    mujeres = Escuela.objects.aggregate(total = Sum('mujeres'))['total']
+    hombres = Escuela.objects.aggregate(total = Sum('hombres'))['total']
+    otros = Escuela.objects.aggregate(total = Sum('otros'))['total']
+
+    #conteos
+    territorios = Escuela.objects.distinct('municipio').count()
+    lideres = Participantes.objects.filter(cargo__nombre = 'Líder o directivo/a').count()
+    formadores = Formadores.objects.count()
+    emprendimientos = Emprendimientos.objects.count()
+
+    return render(request, template, locals()) 
+
+def detalle_nucleo(request,slug,template='nuestro-trabajo/detalle_nucleo.html'):
+    object = Escuela.objects.get(slug=slug)
+    return render(request, template, locals()) 
+
 @login_required
 def formadores(request,template='nuestro-trabajo/formadores.html'):
     object = Formadores.objects.order_by('nombre')
+    return render(request, template, locals())
+
+@login_required
+def participantes(request,template='nuestro-trabajo/participantes.html'):
+    object = Participantes.objects.order_by('nombre')
     return render(request, template, locals())
