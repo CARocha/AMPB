9e4433a2493c9f991ac6044036f002b29b73a2f0
Carlos Rocha <crocha09.09@gmail.com>
2021-08-18 18:01:43 -0600

    try 1 fix search whoosh

--- web/search_indexes.py
+++ web/search_indexes.py
@@ -1,27 +1,32 @@
+import datetime
 from haystack import indexes
 from .models import *
 
 class ActualidadIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    titulo = indexes.NgramField(model_attr='titulo')
-    resumen = indexes.NgramField(model_attr='contenido', null=True)
+    text = indexes.CharField(document=True, use_template=True)
+    titulo = indexes.CharField(model_attr='titulo')
+    resumen = indexes.CharField(model_attr='contenido', null=True)
+    fecha = indexes.DateTimeField(model_attr='fecha')
 
     def get_model(self):
         return Actualidad
 
     def index_queryset(self, using=None):
-        return self.get_model().objects.order_by('-id')
+        #return self.get_model().objects.order_by('-id')
+        return self.get_model().objects.filter(fecha__lte=datetime.datetime.now())
 
 class EventoIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    titulo = indexes.NgramField(model_attr='titulo')
-    descripcion = indexes.NgramField(model_attr='descripcion', null=True)
+    text = indexes.CharField(document=True, use_template=True)
+    titulo = indexes.CharField(model_attr='titulo')
+    descripcion = indexes.CharField(model_attr='descripcion', null=True)
+    fin = indexes.DateTimeField(model_attr='fin')
 
     def get_model(self):
         return Evento
 
     def index_queryset(self, using=None):
-        return self.get_model().objects.order_by('-id')
+        #return self.get_model().objects.order_by('-id')
+        return self.get_model().objects.filter(fin__lte=datetime.datetime.now())
 
 class BibliotecaIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.NgramField(document=True, use_template=True)
